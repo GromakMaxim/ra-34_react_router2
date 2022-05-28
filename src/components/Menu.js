f466703e103c8ac2b86506617e1794d8c59e8a3d
MaximGromak <maximgromak@gmail.com>
2022-05-28 17:32:47 +1000

    create new post

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link, Route, Routes} from "react-router-dom";
 import PostsPage from "./pages/PostsPage";
+import CreatePage from "./pages/CreatePage";
 
 export default function Menu(props){
 
@@ -13,6 +14,7 @@ export default function Menu(props){
 
             <Routes>
                 <Route exact path='/' element={<PostsPage/>}/>
+                <Route exact path='/posts/new' element={<CreatePage/>}/>
             </Routes>
         </>
     );
---
+++ src/components/pages/CreatePage.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import {v4 as uuidv4} from 'uuid';
+import {Link} from "react-router-dom";
+
+export default function CreatePage(props) {
+    const [txt, setTxt] = useState("");
+
+    async function send() {
+        let uuid = await uuidv4();
+        let data = {
+            id: uuid,
+            content: txt
+        }
+
+        let response = await fetch('https://hooks2-api.herokuapp.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        });
+        let result = await response.json();
+        window.location.assign('/');
+        console.log(result)
+    }
+
+    return (
+        <div className='create'>
+            <div className='title'>Создание поста</div>
+            <textarea placeholder='начните писать здесь..' onChange={
+                () => setTxt(document.querySelector('.create > textarea').value)
+            }/>
+            <div className='create_controls'>
+                <button className='cancel' onClick={()=> window.location.assign('/')}>Cancel</button>
+                <button className='ok' onClick={send}>OK</button>
+            </div>
+        </div>
+    );
+
+
+}
